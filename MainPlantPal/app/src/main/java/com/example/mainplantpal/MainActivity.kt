98a4655f5be07f5fdd7ea00b92ac1f67188c034c
devilcoder31 <97335777+devilcoder31@users.noreply.github.com>
2025-06-15 14:08:21 +0530

    Add files via upload

---
+++ MainPlantPal/app/src/main/java/com/example/mainplantpal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mainplantpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedIn   ``                          stanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
